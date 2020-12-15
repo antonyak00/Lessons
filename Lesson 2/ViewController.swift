c4d916c2cc7d036e8c4a8122e88fc48570ae984d
Roman <mr.antonyak00@yandex.ru>
2020-12-15 17:00:40 +0300

    Без комментария

---
+++ Lesson 2/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Lesson 2
+//
+//  Created by Роман Антоняк on 15.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
